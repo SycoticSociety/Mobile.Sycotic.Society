19a89051990ed066f961c5621fd18c59af6dd444
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-01 13:00:45 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Header() {
     return(
         <div className={styles.header}>
-            <Image src={"/Thirdweb-Icon-Circle-Black-08 1.png"} alt="" height={48} width={48}/>
+            <Image src={"/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
             <p>Sycotic Society Moblie Application</p>
         </div>
     )
