2df552c6768ee8a1a33df8c58decc1e1237c03f1
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-03 08:54:06 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Header() {
-    return(
+    return (
         <div className={styles.header}>
             <Image src={"/images/Thirdweb-Icon-Circle-Black-08 1.gif"} alt="" height={60} width={60}/>
-            <p>Sycotic Society Moblie Application</p>
+            <p style={{ fontSize: 'larger' }}>Sycotic Society Mobile Application</p>
         </div>
-    )
+    );
 };
