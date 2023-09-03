4ebdcdceaa2f69a0cd671a095d5f6bdc2fe144a5
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-03 08:02:09 -0400

    Update nft-card.tsx

--- components/nft-card.tsx
+++ components/nft-card.tsx
@@ -14,7 +14,7 @@ export default function NFTCard({ nft, quantity }: NFTProps) {
         <div className={styles.nftCard}>
             <ThirdwebNftMedia
                 metadata={nft.metadata}
-                width="60%"
+                width="50%"
                 height="auto"
             />
             <div className={styles.nftCardContent}>
