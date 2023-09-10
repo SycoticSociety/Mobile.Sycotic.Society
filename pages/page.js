65848a0753f2af5ac7b35d453708a84fcf7a707a
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-10 13:46:33 -0400

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -12,29 +12,10 @@ import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const address = useAddress();
-
-  const { contract } = useContract(NFT_CONTRACT_ADDRESS);
-
-  const { data: contractMetadata } = useContractMetadata(contract);
 
   return (
     <div className={styles.container}>
-      {address ? (
-        <div className={styles.nftClaim}>
-          <MediaRenderer
-            src={contractMetadata?.image}
-            width="auto"
-            height="50%"
-            style={{
-              borderRadius: "20px",
-              maxWidth: "600px",
-            }}
-          />
-          <h1>{contractMetadata?.name}</h1>
-          <ConnectWallet btnTitle="Login" className={styles.connectWalletButton} /> {/* Apply the CSS class here */}
-        </div>
-      ) : (
+    
         <div className={styles.loginContainer}>
           <ConnectWallet btnTitle="Login" className={styles.connectWalletButton} /> {/* Apply the CSS class here */}
         </div>
